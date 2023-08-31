dfa5c3a0ee67902294aa5286c883752974967164
RAFIKHA  SHAHA <rafikshah2272@gmail.com>
2023-08-31 22:34:01 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas
+import sklearn
+import pickle
+
+# importing model
+model = pickle.load(open('model.pkl','rb'))
+
+# creating flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    N = int(request.form['Nitrogen'])
+    P = int(request.form['Phosporus'])
+    K = int(request.form['Potassium'])
+    temp = float(request.form['Temperature'])
+    humidity = float(request.form['Humidity'])
+    ph = float(request.form['Ph'])
+    rainfall = float(request.form['Rainfall'])
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+
+    prediction = model.predict(single_pred)
+
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+    if prediction[0] in crop_dict:
+        crop = crop_dict[prediction[0]]
+        result = "{} is the best crop to be cultivated right there".format(crop)
+    else:
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    return render_template('index.html',result = result)
+
+
+
+
+# python main
+if __name__ == "__main__":
+    app.run(debug=True)
